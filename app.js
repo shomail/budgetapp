cc747cbd9b472615d6ceb4be9243e64356c3f479
Shomail <shomail1988@gmail.com>
2018-11-23 09:17:08 +0100

    constructor functions added in budgetController

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const budgetController = (function() {})();
+const budgetController = (function() {
+
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  const Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  const data = {
+    alItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+})();
 
 const UIController = (function() {
   const DOMstrings = {
