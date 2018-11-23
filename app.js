a42cb33edd1a4056dd09f87d1213f073394ef3ac
Shomail <shomail1988@gmail.com>
2018-11-23 10:34:18 +0100

    UI list mutator function added

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const budgetController = (function() {
   return {
     addItems: function(type, desc, val) {
       let item;
-      
+
       const id = data.allItems[type].length ? data.allItems[type][data.allItems[type].length - 1].id + 1 : 0;
 
       if(type === 'expense'){
@@ -51,7 +51,9 @@ const UIController = (function() {
     inputType: '.add__type',
     inputDesc: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list',
   };
 
   return {
@@ -61,6 +63,41 @@ const UIController = (function() {
         desc: document.querySelector(DOMstrings.inputDesc).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    addListItem: function(obj, type) {
+      let html, className;
+
+      if(type === 'income') {
+        className = DOMstrings.incomeContainer;
+        html = `
+          <div class="item clearfix" id="income-${obj.id}">
+            <div class="item__description">${obj.description}</div>
+            <div class="right clearfix">
+                <div class="item__value">+ ${obj.value}</div>
+                <div class="item__delete">
+                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                </div>
+            </div>
+          </div>
+        `;
+      }else {
+        className = DOMstrings.expenseContainer;
+        html = `
+          <div class="item clearfix" id="expense-${obj.id}">
+            <div class="item__description">${obj.description}</div>
+            <div class="right clearfix">
+                <div class="item__value">- ${obj.value}</div>
+                <div class="item__percentage">21%</div>
+                <div class="item__delete">
+                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                </div>
+            </div>
+          </div>
+        `;
+      }
+
+      document.querySelector(className).insertAdjacentHTML('beforeend', html);
     },
 
     getDOMStrings: function() {
@@ -86,6 +123,8 @@ const controller = (function(budgetCtrl, UICtrl) {
     const input = UICtrl.getInput();
 
     const newItem = budgetCtrl.addItems(input.type, input.desc, input.value);
+
+    UICtrl.addListItem(newItem, input.type);
   };
 
   return {
