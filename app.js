d54b092d55a808f4248b3fac61259f2b2b72d2b8
Shomail <shomail1988@gmail.com>
2018-11-24 07:27:15 +0100

    reset function added

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const budgetController = (function() {
-
   const Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
 
   const Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
 
   const data = {
     allItems: {
@@ -21,7 +20,7 @@ const budgetController = (function() {
       expense: 0,
       income: 0
     }
-  }
+  };
 
   return {
     addItems: function(type, desc, val) {
@@ -29,21 +28,20 @@ const budgetController = (function() {
 
       const id = data.allItems[type].length ? data.allItems[type][data.allItems[type].length - 1].id + 1 : 0;
 
-      if(type === 'expense'){
+      if (type === 'expense') {
         item = new Expense(id, desc, val);
-      }else{
+      } else {
         item = new Income(id, desc, val);
       }
-      
+
       data.allItems[type].push(item);
       return item;
     },
 
-    log: function(){
+    log: function() {
       console.log(data);
     }
-  }
-
+  };
 })();
 
 const UIController = (function() {
@@ -53,7 +51,7 @@ const UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expenseContainer: '.expenses__list',
+    expenseContainer: '.expenses__list'
   };
 
   return {
@@ -68,7 +66,7 @@ const UIController = (function() {
     addListItem: function(obj, type) {
       let html, className;
 
-      if(type === 'income') {
+      if (type === 'income') {
         className = DOMstrings.incomeContainer;
         html = `
           <div class="item clearfix" id="income-${obj.id}">
@@ -81,7 +79,7 @@ const UIController = (function() {
             </div>
           </div>
         `;
-      }else {
+      } else {
         className = DOMstrings.expenseContainer;
         html = `
           <div class="item clearfix" id="expense-${obj.id}">
@@ -98,6 +96,18 @@ const UIController = (function() {
       }
 
       document.querySelector(className).insertAdjacentHTML('beforeend', html);
+    },
+
+    clearFields: function() {
+      const fields = document.querySelectorAll(DOMstrings.inputDesc + ', ' + DOMstrings.inputValue);
+
+      const filedsArr = Array.prototype.slice.call(fields);
+
+      filedsArr.foreach(function(elem) {
+        elem.value = '';
+      });
+
+      filedsArr[0].focus();
     },
 
     getDOMStrings: function() {
@@ -125,6 +135,8 @@ const controller = (function(budgetCtrl, UICtrl) {
     const newItem = budgetCtrl.addItems(input.type, input.desc, input.value);
 
     UICtrl.addListItem(newItem, input.type);
+
+    UICtrl.clearFields();
   };
 
   return {
